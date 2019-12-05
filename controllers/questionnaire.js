d64dc6647cd2cb88fdb4b054d2636a1d41895c51
Lassi Palojärvi <lassi.palojarvi@outlook.com>
2019-12-05 18:30:36 +0200

    added questionnaire controller methods

---
+++ controllers/questionnaire.js
@@ -0,0 +1,40 @@
+"use strict"
+
+const Questionnaire = require('../models/questionnaire');
+module.exports = {
+
+    /**
+     * Returns list of users
+     * @param {Object} request is express request object
+     * @param {Object} response is express response object
+     */
+    async list(request, response) {
+        const questionnaire = await Questionnaire.find()
+            .sort('title')
+            .exec()
+        console.log("listing", questionnaire)
+        response.render("questionnaire/questionnaires")
+    },
+    async show(request, response) {
+
+    },
+    async create(request, response) {
+
+    },
+    async update(request, response) {
+
+    },
+    async delete(request, response) {
+
+    },
+    processCreate(request, response) {
+
+    },
+    processUpdate(request, response) {
+
+    },
+    processDelete(request, response) {
+
+    }
+
+}
