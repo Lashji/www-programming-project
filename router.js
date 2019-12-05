010fb6557ba76cf555579290d861a2e3e326ecbf
Lassi Palojärvi <lassi.palojarvi@outlook.com>
2019-12-05 18:30:13 +0200

    Added questionnaire to router

--- router.js
+++ router.js
@@ -3,9 +3,10 @@
 // load routers
 const UsersRouter = require('./routes/users');
 const HelloRouter = require('./routes/hello');
-
+const QuestionnaireRoute = require("./routes/questionnaire")
 // Setup Routes
-module.exports = function(app) {
+module.exports = function (app) {
     app.use('/users', UsersRouter);
+    app.use('/questionnaire', QuestionnaireRoute)
     app.use('/', HelloRouter);
 };
